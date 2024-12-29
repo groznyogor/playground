f6da1c14e8cf20ee89e83bcf2db754936744be0c
szymek <144553964+groznyogor@users.noreply.github.com>
2024-12-29 16:20:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import Flask
-from Flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+SHIT = ['ogur', 'gogus', 'cwel'] # nice list
 
 @app.route('/')
 def cwel():
@@ -13,7 +14,13 @@ def hi():
 
 @app.route('/404') # goofy ahh 404 (idk working or not)
 def cwelowskagadka():
-  return 'error' # return 'error'
+  return 'error', 404 # return 'error'
+
+@app.route('/api/v1/cwel/shit') # nice endpoint
+def list():
+  return {'cwele': SHIT} # return 'SHIT' list
+
+
 
 if __name__ == '__main__': # ok
     app.run(debug=True)
